50436a36e7b3c245114a6ef2d263aa86c46f0124
oleksij <o.samoilenko95@gmail.com>
2023-10-09 18:48:00 +0300

    homework

---
+++ src/test/java/alertTest/BaseTestClass.java
@@ -0,0 +1,22 @@
+package alertTest;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class BaseTestClass {
+
+    protected WebDriver driver;
+
+    @BeforeClass
+    public void beforeClass(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @AfterClass
+    public void afterClass(){
+        if(driver != null){
+            driver.quit();
+        }
+    }
+}
---
+++ src/test/java/alertTest/homework.java
@@ -0,0 +1,37 @@
+package alertTest;
+import java.util.Scanner;
+
+public class homework {
+
+    public static void main(String[] args) {
+        String DataUsername = "user1";
+        String DataPassword = "123qwe";
+
+        try{
+            Scanner data = new Scanner(System.in);
+            System.out.println("Enter Username: ");
+            String userName = data.nextLine();
+            if (userName == null || userName.isEmpty()){
+                throw new NullPointerException();
+            }
+            System.out.println("enter password: ");
+            String password = data.nextLine();
+            if (password == null || password.isEmpty()) {
+                throw new NullPointerException("Password cant be empty");
+            }
+            if (password.length() < 6){
+                throw new IllegalArgumentException("Password is less then 6");
+            }
+            if (!DataUsername.equals(userName) || !DataPassword.equals(password)){
+                throw new SecurityException("Usernme or Password is not valid");
+            }
+            System.out.println("Login Success!");
+        } catch (NullPointerException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalArgumentException e) {
+            throw new RuntimeException(e);
+        } catch (SecurityException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    }
